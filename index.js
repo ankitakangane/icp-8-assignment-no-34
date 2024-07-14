40eab6102bdd1d4b931e84ce6a066f9855aff4a4
ankitakangane <kanganeankita1@gmail.com>
2024-07-14 17:24:19 +0530

    find the largest number by array

---
+++ index.js
@@ -0,0 +1,20 @@
+let result = [99 , 56 , 70 , 95 ];
+
+let largest = result[0];
+
+for(const num of result){
+    if(num>largest){
+        largest=num;
+    }
+}
+console.log(`Largest Result:${largest}`);
+
+
+
+
+
+
+
+
+
+
